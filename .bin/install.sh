bd91f632e54ce523758fb39512baee38d4cac633
AtsushiNi <45954308+AtsushiNi@users.noreply.github.com>
2020-11-26 16:11:26 +0900

    '表示変更

--- .bin/install.sh
+++ .bin/install.sh
@@ -8,8 +8,6 @@ REMOTE_URL="git@github.com:AtsushiNi/dotfiles.git"
 
 # アプリのインストール
 initialize() {
-  echo "start initialize..."
-
   # brew install
   brew_items=(
     git
@@ -19,9 +17,10 @@ initialize() {
     neovim
     cask
   )
-  echo "brew install apps..."
+  echo $(tput setaf 2)brew install apps...$(tput sgr0)
   for item in "${brew_items[@]}";do
     brew install $item
+    echo $item
   done
 
   # brew cask install(GUI apps)
@@ -31,21 +30,22 @@ initialize() {
     visual-studio-code
     karabiner-elements
   )
-  echo "brew cask install GUI Apps..."
+  echo $(tput setaf 2)brew cask install GUI Apps...$(tput sgr0)
   for item in "${cask_items[@]}";do
     brew cask install $item
+    echo $item
   done
 
   echo "cleanup Homebrew..."
   brew cleanup
-  echo $(tput setaf 2)Cleanup Homebrew completed!$(tput sgr0)
+  echo "Cleanup Homebrew completed!"
 
-  echo $(tput setaf 2)Initialize completed!$(tput sgr0)
+  echo $(tput setaf 2)install apps completed!$(tput sgr0)
 }
 
 # シンボリックリンクの作成(オプションがあればdotfilesの更新も)
 make_symbolic_links() {
-  echo "start deploy..."
+  echo $(tput setaf 2)start making symbolic links...$(tput sgr0)
 
   # dotfilesがなければダウンロード
   if [ ! -d ${DOT_DILECTORY} ]; then
@@ -67,12 +67,12 @@ make_symbolic_links() {
     ln -snfv ${DOT_DILECTORY}/${f} ${HOME}/${f}
   done
 
-  echo $(tput setaf 2)Deploy dotfiles completed!$(tput sgr0)
+  echo $(tput setaf 2)making symbolic links completed!$(tput sgr0)
 }
 
 # Homebrewインストール
 if [ which brew >/dev/null 2>&1 ];then
-  echo "install homebrew..."
+  echo $(tput setaf 2)install homebrew...$(tput sgr0)
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
   echo "update homebrew..."
